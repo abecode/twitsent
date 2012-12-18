d5470e270a64206d5ea41683be21f11c8096ff79
abe <kazemzad@usc.edu>
2012-12-17 20:04:50 -0800

    first stab orange dashboard, Alex style

---
+++ orange.py
@@ -0,0 +1,19 @@
+from flask import Flask
+from flask import render_template
+import pymongo
+import iso8601
+import random
+#from config import *
+
+app = Flask(__name__)
+
+css = '/static/style.css'
+
+@app.route("/")
+def displayOverview():
+                        
+    return render_template('orangeOverview.html', css=css)
+
+
+if __name__ == '__main__':
+    app.run(host="homebrew.usc.edu", debug=True)
