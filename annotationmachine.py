1aa7a6e33f474685ee33858bd366b1100c46a049
abe <kazemzad@usc.edu>
2013-01-09 18:28:51 -0800

    started the annotation machine

--- annotationmachine.py
+++ annotationmachine.py
@@ -1,7 +1,15 @@
 #!/usr/bin/python
 
+from statemachine import StateMachine, State
+
+class AnnotationMachine(StateMachine):
+    welcomeState = State("welcome")
+    loginState = State("login")
+    annotateState = State("annotate")
+    endState = State("end")
 
 
 if __name__ == "__main__":
     import doctest
     doctest.testmod()
+    
